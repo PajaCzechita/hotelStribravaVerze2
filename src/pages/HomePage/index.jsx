8fa171e49db09bcce94ac8b59a060b32c6e22ecd
Pavlína Bajgarová <p.bajgarova@gmail.com>
2025-05-30 16:11:40 +0200

    Create Header component

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,17 +1,18 @@
-import './style.css';
+import { Header } from "../../components/Header";
+import "./style.css";
 
 export const HomePage = () => {
   return (
     <div className="container">
       <header>
-        <div className="logo" />
-        <h1>React webová aplikace</h1>
+        <Header />
       </header>
       <main>
         <p>
-          Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí
-          {" "}
-          <a href="https://www.npmjs.com/package/create-czechitas-app">create-czechitas-app</a>
+          Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí{" "}
+          <a href="https://www.npmjs.com/package/create-czechitas-app">
+            create-czechitas-app
+          </a>
           .
         </p>
       </main>
