f8e840d922b4268379210ea99b530709bde6003d
Pavlína Bajgarová <p.bajgarova@gmail.com>
2025-05-31 14:13:33 +0200

    Add fetch fci to get rooms/api data and pass it to RoomCard component

--- src/components/RoomBooking/index.jsx
+++ src/components/RoomBooking/index.jsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react";
+import { RoomCard } from "../RoomCard";
+
 export const RoomBooking = () => {
+  const [rooms, setRooms] = useState([]);
+
+  useEffect(() => {
+    const fetchRooms = async () => {
+      const response = await fetch("http://localhost:4000/api/rooms");
+      const responseData = await response.json();
+      setRooms(responseData.data);
+    };
+    fetchRooms();
+  }, []);
+
   return (
-    <section class="dark">
-      <div class="container">
+    <section className="dark">
+      <div className="container">
         <h2>Nase Pokoje</h2>
         <p>Vyberte si, ktery z nasich pokoju je pro vas ten pravy</p>
+        <RoomCard rooms={rooms} />
       </div>
     </section>
   );
